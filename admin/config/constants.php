3fd87bfa7a08213c2dbda85bc221b732a6b2cbf2
MRxwr <nasserhatab@gmail.com>
2025-08-26 19:01:17 +0300

    save

--- admin/config/constants.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	define('SITEURL', 'https://myshootskw.net/');
-	define('LOCALHOST', 'localhost');
-	define('USERNAME', 'u671249433_myshootDemoUR');
-	//define('PASSWORD', 'Cre@1ebhadra');
-	define('PASSWORD', 'N@b$90949089');
-	define('DBNAME', 'u671249433_myshootDemoDb');
-?>
