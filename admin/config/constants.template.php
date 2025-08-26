042b5d29e8d8c1ab3a9e3d7ae45a1358ef647717
MRxwr <nasserhatab@gmail.com>
2025-08-26 19:02:04 +0300

    Add .gitignore to exclude sensitive credentials

---
+++ admin/config/constants.template.php
@@ -0,0 +1,7 @@
+<?php 
+	define('SITEURL', 'your-site-url-here');
+	define('LOCALHOST', 'localhost');
+	define('USERNAME', 'your-database-username');
+	define('PASSWORD', 'your-database-password');
+	define('DBNAME', 'your-database-name');
+?>
