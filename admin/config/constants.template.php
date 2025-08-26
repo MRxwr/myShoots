048e86b9bfb9b9dbeb201db40facd5c3d961d6fe
MRxwr <nasserhatab@gmail.com>
2025-08-26 19:02:36 +0300

    save

--- admin/config/constants.template.php
+++
@@ -1,7 +0,0 @@
-<?php 
-	define('SITEURL', 'your-site-url-here');
-	define('LOCALHOST', 'localhost');
-	define('USERNAME', 'your-database-username');
-	define('PASSWORD', 'your-database-password');
-	define('DBNAME', 'your-database-name');
-?>
