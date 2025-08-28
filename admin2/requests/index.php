d5959398f636bbd0281fbbf49724e34c732da8ef
MRxwr <nasserhatab@gmail.com>
2025-08-28 21:35:52 +0300

    Update index.php

--- admin2/requests/index.php
+++ admin2/requests/index.php
@@ -1,11 +1,9 @@
 <?php 
 require_once("../includes/checksouthead.php");
 
-var_dump($_REQUEST);
-echo "requests/{$_GET["f"]}/" . searchFile("{$_GET["f"]}","api{$_GET["endpoint"]}.php"); die();
 // get viewed apis from requests folder \\
 if( isset($_GET["endpoint"]) && isset($_GET["f"]) && searchFile("{$_GET["f"]}","api{$_GET["endpoint"]}.php") ){
-	require_once("requests/{$_GET["f"]}/".searchFile("{$_GET["f"]}","api{$_GET["endpoint"]}.php"));
+	require_once("{$_GET["f"]}/".searchFile("{$_GET["f"]}","api{$_GET["endpoint"]}.php"));
 }else{
 	echo outputError(array("msg"=>"Invalid request"));
 }
