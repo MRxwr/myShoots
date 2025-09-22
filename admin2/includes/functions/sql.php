51f6baeeba43e70640394b212710e7141647eb60
MRxwr <nasserhatab@gmail.com>
2025-09-23 02:03:58 +0300

    Update sql.php

--- admin2/includes/functions/sql.php
+++ admin2/includes/functions/sql.php
@@ -206,7 +206,8 @@ function insertDB($table, $data){
     $sql = rtrim($sql, ",");
     $placeholders = rtrim($placeholders, ",");
     $sql .= ") VALUES ({$placeholders})";
-    echo " we are here ";
+    var_dump($dbconnect);
+    echo " we are here db";
     $stmt = $dbconnect->prepare($sql);
     $types = str_repeat('s', count($data));
     $stmt->bind_param($types, ...array_values($data));
