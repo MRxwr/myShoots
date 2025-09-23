c80e7b17686f0a38eeac449a2d9be2dd83b454e9
MRxwr <nasserhatab@gmail.com>
2025-09-24 01:42:58 +0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -366,7 +366,7 @@ function checkDateAndRedirect(date, redirectUrl) {
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
     },
-    body: 'date=' + encodeURIComponent(date)
+    body: 'package_id=' + encodeURIComponent(<?php echo $_GET["id"]; ?>) + '&date=' + encodeURIComponent(date)
   })
   .then(response => response.json())
   .then(data => {
