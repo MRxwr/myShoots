5159552a4ca8b1994f8b92b751279e78fda9676c
MRxwr <nasserhatab@gmail.com>
2025-09-24 01:22:02 +0300

    Update apiCheckDate.php

--- requests/views/apiCheckDate.php
+++ requests/views/apiCheckDate.php
@@ -5,15 +5,10 @@ if( isset($_REQUEST["date"]) && !empty($_REQUEST["date"]) ){
     $closeDate = $settings[0]["close_date"];
     $userDate = $_REQUEST["date"];
     $selectedDate = date('Y-m-d', strtotime(str_replace('/', '-', $userDate)));
-    // Convert all dates to timestamps
-    $selectedTimestamp = strtotime($selectedDate);
-    $openTimestamp = strtotime($openDate);
-    $closeTimestamp = strtotime($closeDate);
-    $debugInfo = "selectedDate: $selectedDate, openDate: $openDate, closeDate: $closeDate | selectedTimestamp: $selectedTimestamp, openTimestamp: $openTimestamp, closeTimestamp: $closeTimestamp";
-    if( ($selectedTimestamp >= $openTimestamp) && ($selectedTimestamp <= $closeTimestamp) ){
+    if( ($selectedDate >= $openDate) && ($selectedDate <= $closeDate) ){
         echo outputData(array("message"=>"Valid date"));
     }else{
-        echo outputError(array("message"=>"$debugInfo | Invalid date, Please select date within the allowed period."));
+        echo outputError(array("message"=>"Invalid date, Please select date within the allowed period."));
     }
 }else{
     echo outputError(array("message"=>"Please provide a date."));
