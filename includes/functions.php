3e1ae6f638bcafd5b46e6682a2183db9dc3769ab
MRxwr <nasserhatab@gmail.com>
2025-09-25 02:08:29 +0300

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -275,7 +275,7 @@ function sendkwtemail($email,$message){
 function get_bookingTimeBydate($id,$date){
 	GLOBAL $obj,$conn;
 	$tbl_name = 'tbl_booking';
-	$where = "`booking_date` LIKE '{$date}' AND `booking_date` NOT LIKE '%0000-00-00%' AND ( `status` = 'Yes' OR `status` = '' AND TIMESTAMPDIFF(MINUTE, `created_at`, CONVERT_TZ(NOW(), '+00:00', '+03:00')) < 10 )";
+	$where = "`booking_date` LIKE '{$date}' AND `booking_date` NOT LIKE '%0000-00-00%' AND ( `status`='Yes' OR ( `status`='' AND TIMESTAMPDIFF(MINUTE, `created_at`, CONVERT_TZ(NOW(), '+00:00', '+03:00')) < 10 ) )";
 	$query = $obj->select_data($tbl_name,$where);
 	$res = $obj->execute_query($conn,$query);
 	if($res == true)
