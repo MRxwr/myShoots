e4e6c792d8010fca0494dc989c29e4c9a16f074f
MRxwr <nasserhatab@gmail.com>
2025-10-02 00:44:00 +0300

    Update bladeBookingPackages.php

--- admin2/views/bladeBookingPackages.php
+++ admin2/views/bladeBookingPackages.php
@@ -267,7 +267,7 @@ if( isset($_POST["arTitle"]) ){
 	<table class="table display responsive product-overview mb-30" id="myTable">
 		<thead>
 		<tr>
-		<th>#</th>
+		<th><?php echo direction("Rank","الترتيب") ?></th>
 		<th><?php echo direction("Logo","الشعار") ?></th>
 		<th><?php echo direction("English Title","العنوان بالإنجليزي") ?></th>
 		<th><?php echo direction("Arabic Title","العنوان بالعربي") ?></th>
