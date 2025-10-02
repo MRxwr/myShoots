4c74a0d1d0be2abb794cb4681a8212bb8f45b1cd
MRxwr <nasserhatab@gmail.com>
2025-10-03 02:07:25 +0300

    Update insta.php

--- views/insta.php
+++ views/insta.php
@@ -9,29 +9,28 @@ include('../admin2/includes/translate.php');
 <html>
 
 <head>
-    <!-- Site made with Mobirise Website Builder v4.12.3, https://mobirise.com -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="generator" content="Mobirise v4.12.3, mobirise.com">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
-    <link rel="shortcut icon" href="<?php echo SITEURL; ?>assets/images/logo.png" type="image/x-icon">
+    <link rel="shortcut icon" href="assets/images/logo.png" type="image/x-icon">
     <meta name="description" content="">
-    <link rel="stylesheet" href="<?php echo SITEURL; ?>assets/et-line-font-plugin/style.css">
-    <link rel="stylesheet" href="<?php echo SITEURL; ?>assets/web/assets/mobirise-icons/mobirise-icons.css">
+    <link rel="stylesheet" href="assets/et-line-font-plugin/style.css">
+    <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,400i,700,700i">
     <link rel="stylesheet" href="assets/bootstrap-material-design-font/css/material.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Arvo:400,400i,700,700i">
-    <link rel="stylesheet" href="<?php echo SITEURL; ?>assets/tether/tether.min.css">
-    <link rel="stylesheet" href="<?php echo SITEURL; ?>assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?php echo SITEURL; ?>assets/socicon/css/socicon.min.css">
-    <link rel="stylesheet" href="<?php echo SITEURL; ?>assets/colorm-icons/style.css">
-    <link rel="stylesheet" href="<?php echo SITEURL; ?>assets/animatecss/animate.min.css">
-    <link rel="stylesheet" href="<?php echo SITEURL; ?>assets/instagram-feed/style.css">
-    <link rel="stylesheet" href="<?php echo SITEURL; ?>assets/slick/slick.css">
-    <link rel="stylesheet" href="<?php echo SITEURL; ?>assets/theme/css/style.css">
-    <link rel="preload" as="style" href="<?php echo SITEURL; ?>assets/mobirise/css/mbr-additional.css">
-    <link rel="stylesheet" href="<?php echo SITEURL; ?>assets/mobirise/css/mbr-additional.css" type="text/css">
+    <link rel="stylesheet" href="assets/tether/tether.min.css">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/socicon/css/socicon.min.css">
+    <link rel="stylesheet" href="assets/colorm-icons/style.css">
+    <link rel="stylesheet" href="assets/animatecss/animate.min.css">
+    <link rel="stylesheet" href="assets/instagram-feed/style.css">
+    <link rel="stylesheet" href="assets/slick/slick.css">
+    <link rel="stylesheet" href="assets/theme/css/style.css">
+    <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css">
+    <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
 </head>
 
 <body>
@@ -48,14 +47,14 @@ include('../admin2/includes/translate.php');
             </div>
         </div>
     </section>
-    <script src="<?php echo SITEURL; ?>assets/web/assets/jquery/jquery.min.js"></script>
-    <script src="<?php echo SITEURL; ?>assets/tether/tether.min.js"></script>
-    <script src="<?php echo SITEURL; ?>assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="<?php echo SITEURL; ?>assets/smooth-scroll/smooth-scroll.js"></script>
-    <script src="<?php echo SITEURL; ?>assets/viewport-checker/jquery.viewportchecker.js"></script>
-    <script src="<?php echo SITEURL; ?>assets/instagram-feed/index.js"></script>
-    <script src="<?php echo SITEURL; ?>assets/slick/slick.min.js"></script>
-    <script src="<?php echo SITEURL; ?>assets/theme/js/script.js"></script>
+    <script src="assets/web/assets/jquery/jquery.min.js"></script>
+    <script src="assets/tether/tether.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/smooth-scroll/smooth-scroll.js"></script>
+    <script src="assets/viewport-checker/jquery.viewportchecker.js"></script>
+    <script src="assets/instagram-feed/index.js"></script>
+    <script src="assets/slick/slick.min.js"></script>
+    <script src="assets/theme/js/script.js"></script>
     <input name="animation" type="hidden">
 </body>
 
