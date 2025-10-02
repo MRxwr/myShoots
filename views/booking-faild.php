4d6612ed699609ebd7d2298c40f340640af329d4
MRxwr <nasserhatab@gmail.com>
2025-10-03 02:43:54 +0300

    save

--- views/booking-faild.php
+++ views/booking-faild.php
@@ -3,27 +3,16 @@
     <div class="container">
       <div class="row">
         <div class="col-12">
-          <h2 class="shoots-Head2">Your reservation was declined..please try again !! 
+          <h2 class="shoots-Head2"><?php direction("Your reservation was declined..please try again !!","تم رفض حجزك.. يرجى المحاولة مرة اخرى !!") ?>
             <span class="theme-bg ml-2" style="border-radius: 30px; color:#FFF; padding: 4px 7px; font-size: 24px;">
               <i class="fa fa-times"></i>
             </span>
           </h2>
         </div>
         <div class="col-md-10 col-sm-10">
-          
-          <!-- <div class="form-group row">
-            <label for="" class="col-12 col-form-label">Note:</label>
-            <div class="col-12">
-              <ul class="list-unstyled h5">
-                <li>- You'll receive an SMS with you reservation details.</li>
-                <li>- 10 days before the session you'll get a remainder SMS with the studio location.</li>
-                <li>- 10 days before the session to reschedule your reservation.</li>
-              </ul>
-            </div>
-          </div> -->
           <div class="row pt-4">
             <div class="col-sm-7 col-md-6">
-              <a href="<?php echo SITEURL; ?>" class="btn btn-lg btn-outline-primary btn-block btn-rounded">Go to Home Page</a>
+              <a href="<?php echo $settingsWebsite; ?>" class="btn btn-lg btn-outline-primary btn-block btn-rounded"><?php echo direction("Back to Home","الرجوع للرئيسية") ?></a>
             </div>
           </div>
         </div>
