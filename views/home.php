9d0aa865ce789e6703dd46977c6fde3e3a9e5107
MRxwr <nasserhatab@gmail.com>
2025-10-03 01:51:32 +0300

    Update home.php

--- views/home.php
+++ views/home.php
@@ -1,96 +1,90 @@
 
-  <section>
-    <div class="container" style="max-width: 1340px;">
-      <div class="row">
-        <div class="col-12">
-          <h2 class="shoots-Head"><?php echo $lang['our_packages'] ?></h2>
-        </div>
+<section>
+  <div class="container" style="max-width: 1340px;">
+    <div class="row">
+      <div class="col-12">
+        <h2 class="shoots-Head"><?php echo direction("Packages","الباقات") ?></h2>
       </div>
-      <div class="row no-gutters">
-      <?php 
-  $packages=get_packages();
-  foreach($packages as $key=>$package){
+    </div>
+    <div class="row no-gutters">
+    <?php 
+      if ( $packages= selectDB("tbl_packages","`is_active` = 'Yes' ORDER BY `rank` ASC") ) {
+        for( $i = 0; $i < sizeof($packages); $i++ ){
+          $package = $packages[$i];
           $id = $package['id'];
           $price = $package['price'];
           $currency = $package['currency'];
-					$post_title = $package['title_'.$_SESSION['lang']];
-          $post_description = $package['description_'.$_SESSION['lang']];
+          $post_title = $package['title_'.direction('en','ar')];
+          $post_description = $package['description_'.direction('en','ar')];
           $image_url =$package['image_url'];
-					$created_at = $package['created_at'];
-					$is_extra = $package['is_extra']; 
-					$extra_items = $package['extra_items'];
-     ?>
-        <!-- Package Div Start -->
-           <div class="col-md-6 col-sm-6 col-12">
-              <a href="<?php echo "{$settingsWebsite}/?v=reservations&id={$id}"; ?>">
-              <div class="package-card card m-2">
-                <div class="card-body p-2">
-                  <div class="row align-items-center no-gutters">
-                  
-                    <div class="col-lg-7 col-md-8 col-sm-12 order-lg-1 order-md-1 order-sm-2 order-2">
-                    <h5 class="package-head"><?=$post_title?></h5>
-                      <?=$post_description?>
-                      <!-- <?php if($is_extra == 1) { ?>
-                      <h5><?php //echo $lang['extra_charges'] ?></h5>
-                      <ul class="list-unstyled">
-						             <?php 
-                        // $rows = json_decode($extra_items); 
-                        // foreach($rows as $row ){
-                        // echo "<li>- ".$row->item." ".$row->price." KD.</li>";
-                        //}
-                        ?>
-                      </ul>
-                      <?php } ?> -->
-                      <p class="theme-color package-price-tag text-right"><span>Price:</span><span class="ml-2"><?=$price?> <?=$currency?></span></p>
-                    </div>
-                    <div class="col-lg-5 col-md-4 order-lg-3 order-md-2 order-sm-1 order-1">
-                      <img src="<?php echo SITEURL; ?>uploads/images/<?=$image_url?>" class="img-rounded img-fluid d-block mx-auto mb-md-0 mb-3">
-                    </div>
+          $created_at = $package['created_at'];
+          $is_extra = $package['is_extra']; 
+          $extra_items = $package['extra_items'];
+          ?>
+          <!-- Package Div Start -->
+          <div class="col-md-6 col-sm-6 col-12">
+            <a href="<?php echo "?v=reservations&id={$id}"; ?>">
+            <div class="package-card card m-2">
+              <div class="card-body p-2">
+                <div class="row align-items-center no-gutters">
+                
+                  <div class="col-lg-7 col-md-8 col-sm-12 order-lg-1 order-md-1 order-sm-2 order-2">
+                  <h5 class="package-head"><?= $post_title ?></h5>
+                    <?= $post_description ?>
+                    <p class="theme-color package-price-tag text-right">
+                      <span><?php echo direction("Price","السعر") ?>:</span><span class="ml-2"><?= $price ?><?= $currency ?></span></p>
+                  </div>
+                  <div class="col-lg-5 col-md-4 order-lg-3 order-md-2 order-sm-1 order-1">
+                    <img src="logos/<?= $image_url ?>" class="img-rounded img-fluid d-block mx-auto mb-md-0 mb-3">
                   </div>
                 </div>
               </div>
-              </a>
             </div>
-        <!-- Package Div End -->
-  <?php } ?>
-        
-      </div>
+            </a>
+          </div>
+          <!-- Package Div End -->
+      <?php 
+        }
+      }
+      ?>
     </div>
-  </section>
-  <!--Package End-->
-  <section class="pb-0">
-    <div class="container" style="max-width: 1340px;">
-      <div class="row">
-        <div class="col-12">
-          <h2 class="shoots-Head"><?php echo $lang['follow_us'] ?></h2>
-        </div>
-      </div>
-    </div>
-  </section>
-  <iframe name="frame" style="width:100%; min-height:350px;" id="frame" src="<?php echo SITEURL; ?>pages/insta.php" allowtransparency="true" frameborder="0"></iframe>
+  </div>
+</section>
+<!--Package End-->
 
-  <section class="pb-0">
-    <div class="container" style="max-width: 1340px;">
-      <div class="row">
-        <div class="col-12">
-          <h2 class="shoots-Head"><?php echo $lang['about_us'] ?></h2>
-        </div>
+<section class="pb-0">
+  <div class="container" style="max-width: 1340px;">
+    <div class="row">
+      <div class="col-12">
+        <h2 class="shoots-Head"><?php echo $lang['follow_us'] ?></h2>
       </div>
     </div>
-    <div class="container-fluid p-0 bg-light">
-      <div class="row no-gutters align-items-center">
-        <div class="col-md-7">
-          <img src="<?php echo SITEURL; ?>assets/img/shoots-about.png" class="img-fluid d-block mx-auto">
-        </div>
-        <div class="col-md-5 p-3 p-md-5">
-          <h2 class="mb-5">Photography</h2>
-          <h5 class="mb-4">Creative Photography Theme</h5>
-                <?php 
-               $about=get_page_details(7);
-               ?>
-          <p class="about-para"><?php echo $about['description_'.$_SESSION['lang']]; ?></p>
-          <a href="<?php echo SITEURL; ?>index.php?page=galleries" class="btn btn-lg btn-outline-secondary px-5 mt-5">Gallery</a>
-        </div>
+  </div>
+</section>
+<iframe name="frame" style="width:100%; min-height:350px;" id="frame" src="<?php echo SITEURL; ?>pages/insta.php" allowtransparency="true" frameborder="0"></iframe>
+
+<section class="pb-0">
+  <div class="container" style="max-width: 1340px;">
+    <div class="row">
+      <div class="col-12">
+        <h2 class="shoots-Head"><?php echo $lang['about_us'] ?></h2>
       </div>
     </div>
-  </section>
+  </div>
+  <div class="container-fluid p-0 bg-light">
+    <div class="row no-gutters align-items-center">
+      <div class="col-md-7">
+        <img src="<?php echo SITEURL; ?>assets/img/shoots-about.png" class="img-fluid d-block mx-auto">
+      </div>
+      <div class="col-md-5 p-3 p-md-5">
+        <h2 class="mb-5">Photography</h2>
+        <h5 class="mb-4">Creative Photography Theme</h5>
+              <?php 
+              $about=get_page_details(7);
+              ?>
+        <p class="about-para"><?php echo $about['description_'.$_SESSION['lang']]; ?></p>
+        <a href="<?php echo SITEURL; ?>index.php?page=galleries" class="btn btn-lg btn-outline-secondary px-5 mt-5">Gallery</a>
+      </div>
+    </div>
+  </div>
+</section>
