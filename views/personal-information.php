91b1735b67ffd5188884123c143b1fbba1723387
MRxwr <nasserhatab@gmail.com>
2025-10-03 03:00:32 +0300

    Update personal-information.php

--- views/personal-information.php
+++ views/personal-information.php
@@ -2,8 +2,9 @@
 
 if( isset($_GET['id']) && !empty($_GET['id']) ){
   if( $package = selectDBNew("tbl_packages",[intval($_GET['id'])],"`id` = ? AND `status` = '0' AND `hidden` = '1'","") ){
-    var_dump($_REQUEST);
+    
   }else{
+    var_dump($_REQUEST);
     header('location: ?v=home&error='.urlencode(base64_encode(direction("Package not found","الباقة غير موجودة"))));die();
   }
 }else{
