410556a0a2dae0750c99a7fe0f8ee3538528648d
MRxwr <nasserhatab@gmail.com>
2025-10-05 00:45:36 +0300

    Update getBookingDetailsAjax.php

--- views/getBookingDetailsAjax.php
+++ views/getBookingDetailsAjax.php
@@ -23,54 +23,47 @@ foreach ($fields as $field) {
 	$titles[$field['id']] = direction('en', 'ar') == 'en' ? $field['enTitle'] : $field['arTitle'];
 }
 ?>
-<div class="panel panel-default card-view">
-	<div class="panel-heading">
-		<div class="pull-left">
+<div class="row mt-5">
+	<div class="col-12">
+		<ul class="list-unstyled h5">
 			<li class="theme-color"><?php echo direction("Reservation Info","معلومات الحجز") ?></li>
-		</div>
-		<div class="clearfix"></div>
+		</ul>
 	</div>
-	<div class="panel-wrapper">
-		<div class="panel-body">
-			<div class="table-wrap">
-				<div class="table-responsive">
-					<table class="table table-bordered">
-						<tr>
-							<td colspan="2" style="color:<?php echo $statusColor; ?>;font-weight:bold;font-size:18px;text-align:center;">
-								<?php echo $status; ?>
-							</td>
-						</tr>
-						<tr>
-							<th><?php echo direction("Reservation ID","رقم الحجز") ?></th>
-							<td><?php echo htmlspecialchars($booking['transaction_id']); ?></td>
-						</tr>
-						<tr>
-							<th><?php echo direction("Package Choosen","الباقة المختارة") ?>:</th>
-							<td><?php echo htmlspecialchars($post_title); ?></td>
-						</tr>
-						<tr>
-							<th><?php echo direction("Date","التاريخ") ?>:</th>
-							<td><?php echo htmlspecialchars($booking_date); ?></td>
-						</tr>
-						<tr>
-							<th><?php echo direction("Time","الوقت") ?>:</th>
-							<td><?php echo htmlspecialchars($booking_time); ?></td>
-						</tr>
-						<?php
-						if ($personalInfo && is_array($personalInfo)) {
-							echo '<tr><th>'.direction("Personal Info","معلومات العميل").':</th><td>';
-							foreach ($personalInfo as $key => $value) {
-								$title = isset($titles[$key]) ? $titles[$key] : $key;
-								echo '<div><strong>'.htmlspecialchars($title).':</strong> '.htmlspecialchars($value).'</div>';
-							}
-							echo '</td></tr>';
-						}
-						?>
-					</table>
-				</div>
-			</div>
-		</div>
-	</div>
+</div>
+<div class="table-responsive">
+	<table class="table table-bordered">
+		<tr>
+			<td colspan="2" style="color:<?php echo $statusColor; ?>;font-weight:bold;font-size:18px;text-align:center;">
+				<?php echo $status; ?>
+			</td>
+		</tr>
+		<tr>
+			<th><?php echo direction("Reservation ID","رقم الحجز") ?></th>
+			<td><?php echo htmlspecialchars($booking['transaction_id']); ?></td>
+		</tr>
+		<tr>
+			<th><?php echo direction("Package Choosen","الباقة المختارة") ?>:</th>
+			<td><?php echo htmlspecialchars($post_title); ?></td>
+		</tr>
+		<tr>
+			<th><?php echo direction("Date","التاريخ") ?>:</th>
+			<td><?php echo htmlspecialchars($booking_date); ?></td>
+		</tr>
+		<tr>
+			<th><?php echo direction("Time","الوقت") ?>:</th>
+			<td><?php echo htmlspecialchars($booking_time); ?></td>
+		</tr>
+		<?php
+		if ($personalInfo && is_array($personalInfo)) {
+			echo '<tr><th>'.direction("Personal Info","معلومات العميل").':</th><td>';
+			foreach ($personalInfo as $key => $value) {
+				$title = isset($titles[$key]) ? $titles[$key] : $key;
+				echo '<div><strong>'.htmlspecialchars($title).':</strong> '.htmlspecialchars($value).'</div>';
+			}
+			echo '</td></tr>';
+		}
+		?>
+	</table>
 </div>
                         
   
