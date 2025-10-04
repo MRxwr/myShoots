a580c707c3e0290442fef48d2d37b0aa6c8bcd96
MRxwr <nasserhatab@gmail.com>
2025-10-04 18:17:23 +0300

    Update payment.php

--- admin2/includes/functions/payment.php
+++ admin2/includes/functions/payment.php
@@ -356,7 +356,7 @@ function createAPI($BookingDetails){
 		"invoiceItems"         => $BookingDetails['InvoiceItems'],
 	);
 	$headers = ['Content-Type: application/json'];
-	var_dump($postMethodLines);die();
+	
 	for ($i = 0; $i < 3; $i++) {
 		$curl = curl_init();
 		curl_setopt_array($curl, array(
@@ -371,6 +371,7 @@ function createAPI($BookingDetails){
 		$response = curl_exec($curl);
 		curl_close($curl);
 		$resultMY = json_decode($response, true);
+		var_dump($resultMY);die();
 		if ( !empty($resultMY["data"]["InvoiceId"]) ) {
 			// Build DB insert data
 			unset($BookingDetails['InvoiceItems']);
