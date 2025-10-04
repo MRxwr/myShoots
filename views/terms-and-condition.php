8b34f466ad495a1cd905335528bc0253f5d7d727
MRxwr <nasserhatab@gmail.com>
2025-10-05 00:24:37 +0300

    Update terms-and-condition.php

--- views/terms-and-condition.php
+++ views/terms-and-condition.php
@@ -12,7 +12,7 @@ if ( $terms = selectDB("tbl_pages", "`id` = '9'") ){
   <div class="container">
     <div class="row">
       <div class="col-12">
-        <h2 class="shoots-Head"><?php echo $terms[direction("ar","en")]."Title" ?></h2>
+        <h2 class="shoots-Head"><?php echo $terms[direction("ar","en")."Title"] ?></h2>
       </div>
       <div class="col-lg-12">
         <?php echo "<p>".urldecode($terms[direction("en","ar")."Details"])."</p>";?>
