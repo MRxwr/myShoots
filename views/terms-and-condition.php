4c434c9a7ac252acb32aa0f697cbba2041d7e0c6
MRxwr <nasserhatab@gmail.com>
2025-10-05 00:08:11 +0300

    save

--- views/terms-and-condition.php
+++ views/terms-and-condition.php
@@ -1,19 +1,21 @@
+<?php
+if ( $terms = get_page_details(9) ){
+}else{
+    $terms = array(
+        direction("en","ar")."Details" => '',
+        direction("ar","en")."Title" => '',
+    );
+}
+?>
 <section>
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <h2 class="shoots-Head"><?php echo direction("Terms and Conditions","الشروط والاحكام") ?></h2>
-        </div>
-        <div class="col-lg-12">
-          <p>
-                <?php 
-              $tearm=get_page_details(9);
-			        echo $tearm['description_'.direction("en","ar")];
-               ?>
-               
-               </p>
-        </div>
-        
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h2 class="shoots-Head"><?php echo $terms[direction("ar","en")]."Title" ?></h2>
+      </div>
+      <div class="col-lg-12">
+        <?php echo "<p>{$terms[direction("en","ar")."Details"]}</p>";?>
       </div>
     </div>
-  </section>
+  </div>
+</section>
