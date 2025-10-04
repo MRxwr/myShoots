f420b8b07b0c60bd48829a9d03c15f881e7f81a5
MRxwr <nasserhatab@gmail.com>
2025-10-05 00:40:30 +0300

    Update getBookingDetailsAjax.php

--- views/getBookingDetailsAjax.php
+++ views/getBookingDetailsAjax.php
@@ -1,118 +1,76 @@
 <?php 
-include('../languages/lang_config.php');
-include('../admin/config/apply.php');
-include('../includes/functions.php');
+require_once("../admin2/includes/config.php");
+require_once("../admin2/includes/functions.php");
+require_once("../admin2/includes/translate.php");
 
-if(isset($_POST['searchquery']) &&( $_POST['searchquery'] != "")){
-  $searchquery = $_POST['searchquery'];
-  $searches=get_bookingSearch($searchquery);
-}		
+if( isset($_POST['searchquery']) && !empty($_POST['searchquery']) && $booking = selectDBNew("tbl_booking", [$_POST['searchquery']], "`transaction_id` = ?","")[0] ){
+}else{
+	echo "<tr><td colspan='11' class='error'>No Search Data Found.</td></tr>";die();
+}	
 ?>
 
+<?php
+$status = ($booking['status'] === 'Yes') ? direction('Successful Booking','حجز ناجح') : direction('Failed Booking','حجز فاشل');
+$statusColor = ($booking['status'] === 'Yes') ? 'green' : 'red';
+$package = get_packages_details($booking['package_id']);
+$post_title = $package[direction('en','ar').'Title'];
+$booking_date = $booking['booking_date'];
+$booking_time = $booking['booking_time'];
+$personalInfo = json_decode($booking['personal_info'], true);
+$fields = selectDB('tbl_personal_info', "`id` != '0'");
+$titles = array();
+foreach ($fields as $field) {
+	$titles[$field['id']] = direction('en', 'ar') == 'en' ? $field['enTitle'] : $field['arTitle'];
+}
+?>
 <div class="panel panel-default card-view">
-							<div class="panel-heading">
-								<div class="pull-left">
-									<h2 class="shoots-Head"><?php echo $lang['search_result'] ?></h2>
-								</div>
-								<div class="clearfix"></div>
-							</div>
-							<div class="panel-wrapper">
-								<div class="panel-body">
-									<div class="table-wrap">
-										<div class="table-responsive">
-											<table id="datable_1" class="table table-hover display  pb-30" >
-												<thead>
-													<tr>
-                                                        <th><?php echo $lang['sn'] ?></th>
-                                                        <th><?php echo $lang['package_name'] ?></th>
-                                                        <th><?php echo $lang['customer_name'] ?></th>
-                                                        <th><?php echo $lang['mobile_number'] ?></th>
-                                                        <th><?php echo $lang['baby_name'] ?></th>
-                                                        <th><?php echo $lang['baby_age'] ?></th>
-                                                        <th><?php echo $lang['instructions'] ?></th>
-                                                        <th><?php echo $lang['booking_date'] ?></th>
-                                                        <th><?php echo $lang['extra_items'] ?></th>
-                                                        <th><?php echo $lang['booking_time'] ?></th>
-                                                        <th><?php echo $lang['booking_price'] ?></th>
-                                                        <th><?php echo $lang['transaction_id'] ?></th>
-													</tr>
-												</thead>
-												<tfoot>
-													<tr>
-                                                        <th><?php echo $lang['sn'] ?></th>
-                                                        <th><?php echo $lang['package_name'] ?></th>
-                                                        <th><?php echo $lang['customer_name'] ?></th>
-                                                        <th><?php echo $lang['mobile_number'] ?></th>
-                                                        <th><?php echo $lang['baby_name'] ?></th>
-                                                        <th><?php echo $lang['baby_age'] ?></th>
-                                                        <th><?php echo $lang['instructions'] ?></th>
-                                                        <th><?php echo $lang['booking_date'] ?></th>
-                                                        <th><?php echo $lang['booking_time'] ?></th>
-                                                        <th><?php echo $lang['extra_items'] ?></th>
-                                                        <th><?php echo $lang['booking_price'] ?></th>
-                                                        <th><?php echo $lang['transaction_id'] ?></th>
-													</tr>
-												</tfoot>
-												<tbody>
-													<?php 
-													if(count($searches) != 0){
-													$sn = 1;
-													foreach($searches as $key=>$searche){ 
-													$package_id = $searche['package_id'];
-															$transaction_id = $searche['transaction_id'];
-															$customer_name = $searche['customer_name'];
-															$mobile_number = $searche['mobile_number'];
-															$baby_name = $searche['baby_name'];
-															$baby_age = $searche['baby_age'];
-															$instructions = $searche['instructions'];
-															$booking_date = $searche['booking_date'];
-															$booking_time = $searche['booking_time'];
-															$extra_items = $searche['extra_items'];
-															$booking_price = $searche['booking_price'];
-															$is_active = $searche['status'];
-															$tbl_name1 = 'tbl_packages';
-															$where = 'id='.$package_id;
-												            $query1 = $obj->select_data($tbl_name1,$where);
-															$res1 = $obj->execute_query($conn,$query1);
-															$row = $obj->fetch_data($res1);
-															$package_name = $row['title_'.$_SESSION['lang']];
-													?>
-													<tr>
-																<td><?php echo $sn++; ?>. </td>
-																<td><?php echo $package_name; ?></td>
-																<td><?php echo $customer_name; ?></td>
-                                                                <td><?php echo $mobile_number; ?></td>
-                                                                <td><?php echo $baby_name; ?></td>
-                                                                <td><?php echo $baby_age; ?></td>
-                                                                <td><?php echo $instructions; ?></td>
-																<td><?php echo $booking_date; ?></td>
-																<td><?php echo $booking_time; ?></td>
-                                                                 <td>
-                                                                    <ul class="list-unstyled">
-                                                                    <?php 
-																	if($extra_items != ""){
-                                                                    $rows = json_decode($extra_items); 
-                                                                    foreach($rows as $row ){
-                                                                        echo "<li>- ".$row->item." ".$row->price." KD.</li>";
-                                                                    	}
-																	}
-                                                                    ?>
-                                                                    </ul>
-                                                                </td>
-                                                                <td>$<?php echo $booking_price; ?> KD</td>
-                                                                <td><?php echo $transaction_id; ?></td>
-															</tr>
-                                                    <?php }
-													} else { 
-														echo "<tr><td colspan='11' class='error'>No Search Data Found.</td></tr>";
-													}
-													?>
-												</tbody>
-											</table>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
+	<div class="panel-heading">
+		<div class="pull-left">
+			<h2 class="shoots-Head"><?php echo direction("Search Result","نتيجة البحث") ?></h2>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+	<div class="panel-wrapper">
+		<div class="panel-body">
+			<div class="table-wrap">
+				<div class="table-responsive">
+					<table class="table table-bordered">
+						<tr>
+							<td colspan="2" style="color:<?php echo $statusColor; ?>;font-weight:bold;font-size:18px;text-align:center;">
+								<?php echo $status; ?>
+							</td>
+						</tr>
+						<tr>
+							<th><?php echo direction("Reservation ID","رقم الحجز") ?></th>
+							<td><?php echo htmlspecialchars($booking['transaction_id']); ?></td>
+						</tr>
+						<tr>
+							<th><?php echo direction("Package Choosen","الباقة المختارة") ?>:</th>
+							<td><?php echo htmlspecialchars($post_title); ?></td>
+						</tr>
+						<tr>
+							<th><?php echo direction("Date","التاريخ") ?>:</th>
+							<td><?php echo htmlspecialchars($booking_date); ?></td>
+						</tr>
+						<tr>
+							<th><?php echo direction("Time","الوقت") ?>:</th>
+							<td><?php echo htmlspecialchars($booking_time); ?></td>
+						</tr>
+						<?php
+						if ($personalInfo && is_array($personalInfo)) {
+							echo '<tr><th>'.direction("Personal Info","معلومات العميل").':</th><td>';
+							foreach ($personalInfo as $key => $value) {
+								$title = isset($titles[$key]) ? $titles[$key] : $key;
+								echo '<div><strong>'.htmlspecialchars($title).':</strong> '.htmlspecialchars($value).'</div>';
+							}
+							echo '</td></tr>';
+						}
+						?>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
                         
   
