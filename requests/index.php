857fd9e5d675c441c9ad00bfec91916e61e406db
MRxwr <nasserhatab@gmail.com>
2025-10-05 01:31:55 +0300

    Update index.php

--- requests/index.php
+++ requests/index.php
@@ -1,6 +1,7 @@
 <?php 
 require_once("../admin2/includes/config.php");
 require_once("../admin2/includes/functions.php");
+require_once("../admin2/includes/translate.php");
 
 // get viewed apis from requests folder \\
 if( isset($_GET["endpoint"]) && isset($_GET["f"]) && searchFile("{$_GET["f"]}","api{$_GET["endpoint"]}.php") ){
