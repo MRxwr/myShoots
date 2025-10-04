17debe5718afb471e9b5038cdc03468b79589050
MRxwr <nasserhatab@gmail.com>
2025-10-04 11:28:28 +0300

    Update reservations.php

--- views/reservations.php
+++ views/reservations.php
@@ -5,8 +5,8 @@ if( isset($_GET['id']) && !empty($_GET['id']) && is_numeric($_GET['id']) ){
     $id = $package['id'];
     $price = $package['price'];
     $currency = $package['currency'];
-    $post_title = $package['title_'.direction("en","ar")];
-    $post_description = $package['description_'.direction("en","ar")];
+    $post_title = $package[direction("en","ar").'title'];
+    $post_description = $package[direction("en","ar").'description'];
     $image_url = $package['imageurl'];
     $created_at = $package['created_at'];
     $is_extra = $package['is_extra']; 
