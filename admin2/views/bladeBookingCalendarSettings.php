42e0d9fbe257a82b088b7cabaacb35af6b085090
MRxwr <nasserhatab@gmail.com>
2025-10-08 00:28:29 +0300

    Update bladeBookingCalendarSettings.php

--- admin2/views/bladeBookingCalendarSettings.php
+++ admin2/views/bladeBookingCalendarSettings.php
@@ -36,12 +36,12 @@ if( isset($_POST["openDate"]) ){
 
 			<div class="col-md-4">
 			<label><?php echo direction("Open Calendar","تاريخ الافتتاح") ?></label>
-			<input type="date" name="openDate" class="form-control" required value="<?php echo isset($settings["openDate"]) ? $settings["openDate"] : "" ?>">
+			<input type="date" name="openDate" class="form-control" required value="<?php echo isset($settings["openDate"]) ? substr($settings["openDate"], 0, 10) : "" ?>">
 			</div>
 
 			<div class="col-md-4">
 			<label><?php echo direction("Close Calendar","تاريخ الإغلاق") ?></label>
-			<input type="date" name="closeDate" class="form-control" required value="<?php echo isset($settings["closeDate"]) ? $settings["closeDate"] : "" ?>">
+			<input type="date" name="closeDate" class="form-control" required value="<?php echo isset($settings["closeDate"]) ? substr($settings["closeDate"], 0, 10) : "" ?>">
 			</div>
 
 			<div class="col-md-4">
