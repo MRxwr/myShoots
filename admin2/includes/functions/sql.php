4669dc2887314ad79f2c0fbf160972cc42686f08
MRxwr <nasserhatab@gmail.com>
2025-10-08 04:26:49 +0300

    Update sql.php

--- admin2/includes/functions/sql.php
+++ admin2/includes/functions/sql.php
@@ -81,7 +81,7 @@ function selectDBNew($table, $placeHolders, $where, $order){
         );
         LogsHistory($array);
     }
-    echo $sql;
+    var_dump($dbconnect->prepare($sql));
     if($stmt = $dbconnect->prepare($sql)) {
         $types = str_repeat('s', count($placeHolders));
         $stmt->bind_param($types, ...$placeHolders);
