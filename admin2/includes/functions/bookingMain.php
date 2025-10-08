bf5877ac1707dd2e55883791d0e16fe74e888e03
MRxwr <nasserhatab@gmail.com>
2025-10-08 04:09:41 +0300

    Update bookingMain.php

--- admin2/includes/functions/bookingMain.php
+++ admin2/includes/functions/bookingMain.php
@@ -5,9 +5,9 @@ function get_setting($set){
 			if( $set == 'openDate' || $set == 'closeDate' ){
 				$time = strtotime(substr($data[$set], 0, 10));
 				if( $set == 'closeDate' ){
-					return date("Y-m-d", $time);
+					return date("Y-m-d", strtotime("-1 month", $time));
 				}else{
-					return date("Y-m-d", strtotime(date("Y-m-d")));
+					return date("Y-m-d", strtotime("-1 month", strtotime(date("Y-m-d"))));
 				}
 			}elseif( $set == 'weekend' ){
 				return $data[$set];
