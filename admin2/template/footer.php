3de038462a71728a1f19f78cb7e92dafd474f684
MRxwr <nasserhatab@gmail.com>
2025-10-08 22:41:50 +0300

    save

--- admin2/template/footer.php
+++ admin2/template/footer.php
@@ -62,5 +62,95 @@
 		
 	<!-- Init JavaScript -->
 	<script src="dist/js/init.js?x=1"></script>
+	<script src="assets/vendors/bower_components/fullcalendar/dist/fullcalendar.min.js"></script>
+<script>
+    $(document).ready(function() {
+	'use strict';
+	
+    var drag =  function() {
+        $('.calendar-event').each(function() {
+
+        // store data so the calendar knows to render an event upon drop
+        $(this).data('event', {
+            title: $.trim($(this).text()), // use the element's text as the event title
+            stick: true // maintain when user navigates (see docs on the renderEvent method)
+        });
+
+        
+    });
+    };
+    
+    var removeEvent =  function() {
+		$(document).on('click','.remove-calendar-event',function(e) {
+			$(this).closest('.calendar-event').fadeOut();
+        return false;
+    });
+    };
+    
+    $(".add-event").keypress(function (e) {
+        if ((e.which == 13)&&(!$(this).val().length == 0)) {
+            $('<div class="btn btn-success calendar-event">' + $(this).val() + '<a href="javascript:void(0);" class="remove-calendar-event"><i class="ti-close"></i></a></div>').insertBefore(".add-event");
+            $(this).val('');
+        } else if(e.which == 13) {
+            alert('Please enter event name');
+        }
+        drag();
+        removeEvent();
+    });
+    
+    
+    drag();
+    removeEvent();
+    
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth();
+    var year = date.getFullYear();
+    
+    $('#calendar').fullCalendar({
+       
+            header: {
+                left: 'prev,next today',
+                center: 'title',
+                right: 'month,agendaWeek,agendaDay'
+            },
+            editable: true,
+            droppable: true, // this allows things to be dropped onto the calendar
+            eventLimit: false, // allow "more" link when too many events
+            timeFormat: 'H:mm',
+        	
+        eventMouseover: function (data, event, view) {
+			var tooltip = '<div class="tooltiptopicevent tooltip tooltip-inner" style="width:auto;height:auto;position:absolute;z-index:10001;">' + data.title + '</div>';
+			$("body").append(tooltip);
+            $(this).mouseover(function (e) {
+                $(this).css('z-index', 10000);
+                $('.tooltiptopicevent').fadeIn('500');
+                $('.tooltiptopicevent').fadeTo('10', 1.9);
+            }).mousemove(function (e) {
+                $('.tooltiptopicevent').css('top', e.pageY + 10);
+                $('.tooltiptopicevent').css('left', e.pageX + 20);
+            });
+        },
+        eventRender: function( event, element, view ) {
+        var title = element.find('.fc-title, .fc-list-item-title');          
+        title.html(title.text());
+        },
+        dayClick: function () {
+            tooltip.hide()
+        },
+        eventResizeStart: function () {
+            tooltip.hide()
+        },
+        eventDragStart: function () {
+            tooltip.hide()
+        },
+        viewDisplay: function () {
+            tooltip.hide()
+        },
+			events: <?php echo json_encode($events)?>
+		});
+ 
+});
+</script>
 </body>
 </html>
