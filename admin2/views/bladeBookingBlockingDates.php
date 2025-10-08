ec9b309eaa07a674a1bcafcfabd1ea91c0b6ba2e
MRxwr <nasserhatab@gmail.com>
2025-10-08 19:49:22 +0300

    Update bladeBookingBlockingDates.php

--- admin2/views/bladeBookingBlockingDates.php
+++ admin2/views/bladeBookingBlockingDates.php
@@ -22,7 +22,6 @@ if( isset($_POST["startBlock"]) ){
 	unset($_POST["update"]);
 	$_POST["timeSlots"] = isset($_POST["timeSlots"]) ? json_encode($_POST["timeSlots"]) : '';
 	$_POST["packages"] = isset($_POST["packages"]) ? json_encode($_POST["packages"]) : '';
-	var_dump($_POST);
 	// if packages empty then add all packages ids to the json array
 	if( empty($_POST["packages"]) ){
 		if( $allPackages = selectDB("tbl_packages","`hidden` = '1' AND `status` = '0'") ){
@@ -43,6 +42,7 @@ if( isset($_POST["startBlock"]) ){
 			$_POST["timeSlots"] = json_encode($timeIds);
 		}
 	}
+	var_dump($_POST);
 	if ( $id == 0 ){
 		if( insertDB("tbl_disabled_date", $_POST) ){
 			header("LOCATION: ?v=BookingBlockingDates");
