58cd6b3a317b5d63129c3b77fdc505aba49a1046
MRxwr <nasserhatab@gmail.com>
2025-10-09 16:24:32 +0300

    Update bladeHome.php

--- admin/views/bladeHome.php
+++ admin/views/bladeHome.php
@@ -2,7 +2,7 @@
     <div class="col-12 text-center">
         <img src="../logos/<?php echo $settingslogo ?>" class="img-rounded img-fluid d-block mx-auto" style="border-radius: 12px; width: 300px; height: 150px; object-fit: cover;">
     </div>
-    <div class="col-12 text-center" style="margin-top: 10px; font-size: 22px; font-weight: 600; color: #333;">
+    <div class="col-12 text-center" style="margin-top: 10px; font-size: 22px; font-weight: 600; color: #333;box-shadow: 0px 0px 15px 5px cadetblue;border: 1px #b4b4b4 solid;">
     Welcome To <?php echo $settingsTitle ?> CP
     </div>
 </div>
