bbd5743d39721ecb5a2984f40320f7734cee9c07
MRxwr <nasserhatab@gmail.com>
2025-10-09 16:18:53 +0300

    Update bladeHome.php

--- admin/views/bladeHome.php
+++ admin/views/bladeHome.php
@@ -1,5 +1,8 @@
 <div class="row" style="padding:16px">
-    <div class="col-12">
+    <div class="col-12 text-center">
+        <img src="logos/<?php echo $settingsLogo ?>" class="img-rounded img-fluid d-block mx-auto" style="border-radius: 12px; width: 300px; height: 150px; object-fit: cover;">
+    </div>
+    <div class="col-12 text-center" style="margin-top: 10px; font-size: 22px; font-weight: 600; color: #333;">
     Welcome To <?php echo $settingsTitle ?> CP
     </div>
 </div>
