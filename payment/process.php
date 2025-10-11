096dc11f8cdd0211aab62f13383b24fe2de1e7a1
MRxwr <nasserhatab@gmail.com>
2025-10-11 05:11:29 +0300

    Update process.php

--- payment/process.php
+++ payment/process.php
@@ -21,7 +21,7 @@ if(isset($_POST['submit'])){
 			$paidAmount = isset($paymentData['price']) ? floatval($paymentData['price']) : 0;
 			$booking_price = isset($paymentData['booking_price']) ? floatval($paymentData['booking_price']) : $booking['booking_price'];
 			$totalAmount = floatval($booking_price);
-			$remainingAmount = $totalAmount - $paidAmount;
+			echo $remainingAmount = $totalAmount - $paidAmount;die();
 			$newPaymentData = array(
 				'type' => isset($paymentData['type']) ? $paymentData['type'] : '1',
 				'price' => $paidAmount,
