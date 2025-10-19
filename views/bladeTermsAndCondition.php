c98d8c105f040b464c9a55a18f0b0e1423ad98a4
MRxwr <nasserhatab@gmail.com>
2025-10-19 14:54:40 +0300

    Update bladeTermsAndCondition.php

--- views/bladeTermsAndCondition.php
+++ views/bladeTermsAndCondition.php
@@ -17,9 +17,9 @@ if ( $terms = selectDB("tbl_pages", "`id` = '9'") ){
             <h2 class="shoots-Head mb-4 text-center" style="font-weight:700; color:#333; letter-spacing:1px;">
               <?php echo $terms[direction("en","ar")."Title"] ?>
             </h2>
-            <div class="mb-3" style="font-size:1.1rem; color:#555; line-height:1.7;">
+            
               <?php echo "<p>".urldecode($terms[direction("en","ar")."Details"])."</p>";?>
-            </div>
+          
           </div>
         </div>
       </div>
